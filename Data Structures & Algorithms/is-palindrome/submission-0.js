262cc221d5db31249c2592837801f4df290c40fb
SingingLin <35301233+SingingLin@users.noreply.github.com>
2026-08-22 12:58:17 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const str = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+        return str === str.split("").reverse().join('');
+    }
+}
+// 
